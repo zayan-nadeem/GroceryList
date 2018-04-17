632b8c9a3ba4cd51ce42aafa3dbb8f2b6a221ec0
zayan-nadeem <zayan.nadeem93@gmail.com>
2018-04-17 15:38:59 -0400

    working application

--- components/groceryitems.ctrl.js
+++ components/groceryitems.ctrl.js
@@ -2,14 +2,36 @@
 
 	angular
 	.module('boaml')
-	.controller('groceryitemsCtrl', function($scope, groceryitemsFactory) {
+	.controller('groceryitemsCtrl', function($scope, groceryitemsFactory, $http) {
 	
 		// This is where main logic goes.	
+	$scope.categories = new Object();
+	$scope.groceryitems = new Array();
+
+
+	$http.get('data/pic.json').then(function(res){
+		//console.log(res.data);
+		$scope.categories = res.data;
+	})
 		
 	groceryitemsFactory.getItems().then(function(res){
 		$scope.groceryitems = res.data;
+		updateCat();
 	})	
-		
+	
+	function updateCat() {
+		//console.log(this);
+		$scope.groceryitems.forEach(function(item) {
+			var x = item["category"];
+			console.log(x);
+			//console.log(Object.keys($scope.categories));
+			var link = $scope.categories[x];
+			console.log(link);
+			item["category"] = link;
+			console.log(item["category"]);
+			console.log("----");
+		});
+	}
 
 	});
 
--- components/groceryitems.fac.js
+++ components/groceryitems.fac.js
@@ -7,12 +7,14 @@
 			
 			//Factories use Singleton pattern
 			//Here you create obj, add properties and return
-
 			var returnobj = new Object();
 
+			
+			
 			returnobj.getItems = function() {
 				return $http.get('data/data.json');
 			}
+
 
 			return returnobj;
 
